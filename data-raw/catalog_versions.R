af5d1f70894e07d326dad5c682c9083e861ecdc1
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-07-09 15:23:31 -0400

    catalog version 1.1.2

--- data-raw/catalog_versions.R
+++ data-raw/catalog_versions.R
@@ -2,7 +2,8 @@ catalog_versions <- tibble::tribble(
     ~ version, ~ zenodoid,
     "1.0.0", "17981132",
     "1.1.0", "20821588",
-    "1.1.1", "20859455"
+    "1.1.1", "20859455",
+    "1.1.2", "21284640"
 )
 
 usethis::use_data(catalog_versions, overwrite = TRUE, internal = TRUE)
