96b149c661fe142660d956054ff4e9e0232ecfbd
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-07-09 13:50:59 -0400

    update import script benchmark

--- inst/scripts/hover_bench.R
+++ inst/scripts/hover_bench.R
@@ -1,21 +1,25 @@
 # timings for functions
 library(imageFeatureTCGA)
 
-hov_json_file <- paste0(
-    "https://store.cancerdatasci.org/hovernet/TCGA_OV/json/",
-    "TCGA-23-1121-01Z-00-DX1.E2F25441-32C3-46BF-A845-CB4FA787E8CB.json.gz"
-)
+hov_json_file <- getCatalog("hovernet", "json") |>
+    dplyr::filter(
+        filename == paste0(
+            "TCGA-23-1121-01Z-00-DX1.",
+            "E2F25441-32C3-46BF-A845-CB4FA787E8CB.json.gz"
+        )
+    ) |>
+    getFileURLs()
+
 dest_json <- file.path(tempdir(), basename(hov_json_file))
 download.file(hov_json_file, destfile = dest_json)
 
 # HoverJSON
 microbenchmark::microbenchmark(
-    HoverJSON(dest_json) |> import(),
+    HoverNet(dest_json) |> import(),
     times = 1L
 )
-## 56 secs
+## 40.55 seconds
 
-## file.edit("~/gh/ImageAnalysisR/share_function/import_json.R")
 ## remove non-function code
 source("~/gh/ImageAnalysisR/share_function/import_json.R")
 
